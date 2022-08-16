e32f351c0fc5ec1bac0167a03303eb878aa32127
Ran Nozik <ran@gethelios.dev>
2022-08-16 14:22:50 +0300

    sampler + debug

--- javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/AgentTracerProviderConfigurer.java
+++ javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/AgentTracerProviderConfigurer.java
@@ -8,6 +8,9 @@ package io.opentelemetry.javaagent.tooling;
 import static io.opentelemetry.javaagent.tooling.AgentInstaller.JAVAAGENT_ENABLED_CONFIG;
 
 import com.google.auto.service.AutoService;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.context.Context;
 import io.opentelemetry.exporter.logging.LoggingSpanExporter;
 import io.opentelemetry.instrumentation.api.config.Config;
 import io.opentelemetry.javaagent.tooling.config.AgentConfig;
@@ -15,8 +18,13 @@ import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import io.opentelemetry.sdk.trace.SdkTracerProviderBuilder;
+import io.opentelemetry.sdk.trace.data.LinkData;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
+import io.opentelemetry.sdk.trace.samplers.SamplingResult;
+import io.opentelemetry.sdk.trace.samplers.TraceIdRatioBasedSampler;
 import java.util.Collections;
+import java.util.List;
 
 @AutoService(AutoConfigurationCustomizerProvider.class)
 public class AgentTracerProviderConfigurer implements AutoConfigurationCustomizerProvider {
@@ -39,6 +47,9 @@ public class AgentTracerProviderConfigurer implements AutoConfigurationCustomize
       sdkTracerProviderBuilder.addSpanProcessor(new AddThreadDetailsSpanProcessor());
     }
 
+    sdkTracerProviderBuilder.setSampler(new HeliosSampler());
+    // TODO set -Dotel.javaagent.debug=true when HS_DEBUG is true
+
     maybeEnableLoggingExporter(sdkTracerProviderBuilder);
 
     return sdkTracerProviderBuilder;
@@ -58,4 +69,26 @@ public class AgentTracerProviderConfigurer implements AutoConfigurationCustomize
         .getList("otel.traces.exporter", Collections.emptyList())
         .contains("logging");
   }
+
+  public static class HeliosSampler implements Sampler {
+    private final TraceIdRatioBasedSampler ratioBasedSampler;
+    public HeliosSampler() {
+      // TODO extract from env var HS_SAMPLING_RATIO
+      this.ratioBasedSampler = TraceIdRatioBasedSampler.create(1);
+    }
+
+    @Override
+    public SamplingResult shouldSample(Context context, String s, String s1, SpanKind spanKind,
+        Attributes attributes, List<LinkData> list) {
+      // TODO implement based on Python's sampler
+      return null;
+    }
+
+    @Override
+    public String getDescription() {
+      return "HeliosSampler";
+    }
+  }
 }
+
+
