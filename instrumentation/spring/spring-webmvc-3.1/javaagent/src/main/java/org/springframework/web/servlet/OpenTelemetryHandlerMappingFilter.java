9271e59bf1dd16b52ba4b3b3a5c5f1ec7c0285e5
LironKS <liron@gethelios.dev>
2022-08-30 15:17:46 +0300

    test muzzle

--- instrumentation/spring/spring-webmvc-3.1/javaagent/src/main/java/org/springframework/web/servlet/OpenTelemetryHandlerMappingFilter.java
+++ instrumentation/spring/spring-webmvc-3.1/javaagent/src/main/java/org/springframework/web/servlet/OpenTelemetryHandlerMappingFilter.java
@@ -79,11 +79,11 @@ public class OpenTelemetryHandlerMappingFilter implements Filter, Ordered {
       return;
     }
 
-    ContentCachingResponseWrapper responseWrapper =
-        new ContentCachingResponseWrapper((HttpServletResponse) response);
+    org.springframework.web.servlet.ContentCachingResponseWrapper responseWrapper =
+        new org.springframework.web.servlet.ContentCachingResponseWrapper((HttpServletResponse) response);
 
-    ContentCachingRequestWrapper requestWrapper =
-        new ContentCachingRequestWrapper((HttpServletRequest) request);
+    org.springframework.web.servlet.ContentCachingRequestWrapper requestWrapper =
+        new org.springframework.web.servlet.ContentCachingRequestWrapper((HttpServletRequest) request);
 
     try {
       filterChain.doFilter(requestWrapper, responseWrapper);
@@ -98,8 +98,8 @@ public class OpenTelemetryHandlerMappingFilter implements Filter, Ordered {
   }
 
   private void setAttributes(
-      ContentCachingRequestWrapper requestWrapper,
-      ContentCachingResponseWrapper responseWrapper,
+      org.springframework.web.servlet.ContentCachingRequestWrapper requestWrapper,
+      org.springframework.web.servlet.ContentCachingResponseWrapper responseWrapper,
       Context context)
       throws IOException {
     Span span = Span.fromContext(context);
