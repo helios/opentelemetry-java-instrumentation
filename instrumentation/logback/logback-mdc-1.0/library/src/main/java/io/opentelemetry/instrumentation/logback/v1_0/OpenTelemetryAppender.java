24930789bfda7b0b3ab117dc6af9f0de832bc110
Yochay <yochai@heliosphere.io>
2022-11-21 13:16:59 +0200

    Same for logback library (!= agent)

--- instrumentation/logback/logback-mdc-1.0/library/src/main/java/io/opentelemetry/instrumentation/logback/v1_0/OpenTelemetryAppender.java
+++ instrumentation/logback/logback-mdc-1.0/library/src/main/java/io/opentelemetry/instrumentation/logback/v1_0/OpenTelemetryAppender.java
@@ -5,6 +5,7 @@
 
 package io.opentelemetry.instrumentation.logback.v1_0;
 
+import static io.opentelemetry.instrumentation.api.log.LoggingContextConstants.HELIOS_INSTRUMENTED_INDICATION;
 import static io.opentelemetry.instrumentation.api.log.LoggingContextConstants.SPAN_ID;
 import static io.opentelemetry.instrumentation.api.log.LoggingContextConstants.TRACE_FLAGS;
 import static io.opentelemetry.instrumentation.api.log.LoggingContextConstants.TRACE_ID;
@@ -30,6 +31,10 @@ public class OpenTelemetryAppender extends UnsynchronizedAppenderBase<ILoggingEv
     Span currentSpan = Span.current();
     if (!currentSpan.getSpanContext().isValid()) {
       return event;
+    }
+
+    if (currentSpan.isRecording()) {
+      currentSpan.setAttribute(HELIOS_INSTRUMENTED_INDICATION, "logback");
     }
 
     Map<String, String> eventContext = event.getMDCPropertyMap();
