aeb4dbeb9abe5874c4f9f2b09ed09c18a8bf5c9a
Oren Levy <oren@gethelios.dev>
2023-01-11 11:45:55 +0200

    api/instrumenter/http/SemanticAttributes - make constructor private to avoid warning

--- instrumentation-api-semconv/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/http/SemanticAttributes.java
+++ instrumentation-api-semconv/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/http/SemanticAttributes.java
@@ -11,6 +11,8 @@ import io.opentelemetry.api.common.AttributeKey;
 
 public final class SemanticAttributes {
 
+  private SemanticAttributes() {}
+
   public static final AttributeKey<String> HTTP_REQUEST_BODY = stringKey("http.request.body");
   public static final AttributeKey<String> HTTP_REQUEST_HEADERS = stringKey("http.request.headers");
   public static final AttributeKey<String> HTTP_RESPONSE_BODY = stringKey("http.response.body");
